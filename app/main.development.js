8889eb175a66378f5c9a5f2f2966e5a2b2117258
Josh Dzielak <jdzielak@gmail.com>
2017-01-25 13:04:22 -0800

    No devtools in prod (was just there to debug)

--- app/main.development.js
+++ app/main.development.js
@@ -83,7 +83,7 @@ app.on('ready', async () => {
     mainWindow = null;
   });
 
-  // if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     mainWindow.openDevTools();
     mainWindow.webContents.on('context-menu', (e, props) => {
       const { x, y } = props;
@@ -95,7 +95,7 @@ app.on('ready', async () => {
         }
       }]).popup(mainWindow);
     });
-  // }
+  }
 
   if (process.platform === 'darwin') {
     template = [{
